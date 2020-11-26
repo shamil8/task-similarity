3fe4b3272a5193238fed0c10c0b9b96039c305ca
Shamil Kurbonov <qurbonovshamil@gmail.com>
2020-11-26 17:10:27 +0700

    Added owner score

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ import {
     SYMBOL_TASK_INDEX
 } from './logic/taskAlgorithm.js'
 
-const {ratings, bestMatchIndex} = taskRatingsWithProbability('Пересчитать расходы', .5, .3, 100)
+const data = {
+    name: 'Пересчитать расходы',
+    ownerId: '855eaff9-d0aa-11ea-97bf-0242c0a87005'
+}
 
 const tasks = []
+const ownerRating = .22
+const { ratings, bestMatchIndex } = taskRatingsWithProbability(data.name, .5, .2, 100)
+
 
 ratings.forEach(rating => {
     const taskIndex = rating.target.lastIndexOf(SYMBOL_TASK_INDEX)
+    const task = findTaskByIndex(rating.target.slice(taskIndex + 1))
 
-    taskIndex && tasks.push({ rating: rating.rating, ...findTaskByIndex(rating.target.slice(taskIndex + 1)) })
+    task.rating = rating.rating
+
+    // owner score
+    if (task.owner_id === data.ownerId) { task.rating += ownerRating }
+
+    taskIndex && tasks.push(task)
 })
 
 console.log('chose tasks', tasks)
@@ -19,5 +31,3 @@ console.log('chose tasks', tasks)
 const task = findTaskByIndex(bestMatchIndex)
 
 console.log('bestMatch', task)
-
-// logic for owner
